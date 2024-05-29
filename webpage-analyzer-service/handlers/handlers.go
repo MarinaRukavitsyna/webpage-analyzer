eeec3521ee2b949c9b614ea8cc91ab2a33f7cf5b
Marina Rukavitsyna <marina.rukavitsyna@gmail.com>
2024-05-29 15:25:18 +0200

    :sparkles: define analysis result struct

--- webpage-analyzer-service/handlers/handlers.go
+++ webpage-analyzer-service/handlers/handlers.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"net/http"
+)
+
+type AnalysisRequest struct {
+	URL string `json:"url"`
+}
+
+type AnalysisResult struct {
+	HTMLVersion       string         `json:"htmlVersion"`
+	PageTitle         string         `json:"pageTitle"`
+	Headings          map[string]int `json:"headings"`
+	InternalLinks     int            `json:"internalLinks"`
+	ExternalLinks     int            `json:"externalLinks"`
+	InaccessibleLinks int            `json:"inaccessibleLinks"`
+	ContainsLoginForm bool           `json:"containsLoginForm"`
+	ErrorMessage      string         `json:"error,omitempty"`
+}
+
+func AnalyzePage(w http.ResponseWriter, r *http.Request) {
+
+}
